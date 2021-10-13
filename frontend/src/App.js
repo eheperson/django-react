432dffb42b98e532852f2368ad0cc860d32ac359
eheperson <barannkuzu@gmail.com>
2021-10-13 18:31:02 +0300

    frontend side is initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React,{useState, useEffect} from 'react';
 
-function App() {
+const App =()=> {
+  const [baran, setBaran] = useState([]);
+
+    const getAllData = async() =>{
+      const URL =`http://127.0.0.1:8000/api/todos/`;
+      await axios
+      .get(URL)
+      .then(response=>setBaran(response.data));
+    }
+    console.log(baran);
+
+
+    const postData = async() =>{
+      const URL = `http://127.0.0.1:8000/api/todos/`;
+      await axios
+      .post(URL,{
+        "id": 6,
+        "title": "enivicivokki-6",
+        "description": "eni vokki vici enivi civi vok kivici enicivivo ki enivici",
+        "completed": true,
+      }).then(response => console.log(response));
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div>
+    <button onClick={getAllData}> get</button>
+    <button onClick={postData}> POSt data</button>
+
+  </div>
+  </>
   );
 }
 
